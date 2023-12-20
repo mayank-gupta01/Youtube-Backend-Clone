93ba7d9361493a6f409c77c78b11736e29543b83
Mayank Gupta <mayank.gupta09022001@gmail.com>
2023-12-20 17:57:39 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Err: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listen on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+})();
+*/
