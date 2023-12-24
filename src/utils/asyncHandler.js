61fb16f7ddb02e7d1dd47dfbbe82382a4b290dc1
Mayank Gupta <mayank.gupta09022001@gmail.com>
2023-12-24 16:56:42 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // async handler wrapper using promises
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
