8e868c3dc49bbd1120033a211d730d9d941bf407
Mayank Gupta <mayank.gupta09022001@gmail.com>
2024-02-02 16:40:47 +0530

    Added healthcheck routes and controllers

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playListRouter from "./routes/playlist.routes.js";
+import healthCheckRouter from "./routes/healthcheck.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -33,5 +34,6 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlists", playListRouter);
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export { app };
